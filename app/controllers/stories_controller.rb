2c842e6ec016040afc624633db66d62cefe84463
zhong <afs784512@hotmail.com>
2021-03-08 18:29:06 +0900

    新增文章功能完成

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,21 +1,43 @@
 class StoriesController < ApplicationController
-    before_action :authenticate_user!
 
+    before_action :authenticate_user!
+    before_action :fine_story,only: [:edit, :update, :destroy]
+
+    def index
+        @stories = current_user.stories.order(created_at: :desc)
+    end
+    # 
     def new
         @story = current_user.stories.new
     end
-
+    # 
     def create
         @story = current_user.stories.new(story_params)
 
         if @story.save
-            redirect_to stories_path, notice: '新增成功'
+            redirect_to stories_path, notice: '完了しました'
         else
             render :new
         end
     end
+    # 
+    def edit
+        
+    end
+    # 
+    def update
+        if @story.update(story_params)
+            redirect_to stories_path, notice: '完了しました'
+        else
+            render :edit
+        end
+    end
 
     private
+    def fine_story
+        @story = current_user.stories.find(params[:id])
+    end
+
     def story_params
         params.require(:story).permit(:title, :content)
     end
